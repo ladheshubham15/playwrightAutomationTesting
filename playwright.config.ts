27d1c9a61c68ae1c370ec48b8751facdd2e858fe
ladheshubham <52656896+ladheshubham15@users.noreply.github.com>
2024-06-04 15:00:23 +0530

    Code refinement

--- playwright.config.ts
+++ playwright.config.ts
@@ -12,7 +12,7 @@ import { defineConfig, devices } from '@playwright/test';
 export default defineConfig({
   testDir: './tests',
   
-  timeout: 30 * 1000,
+  timeout: 40 * 1000,
 
   expect:{
     timeout: 5000,
--- support/pages/WikipediaPage.ts
+++ support/pages/WikipediaPage.ts
@@ -1,6 +1,6 @@
 import { BasePage } from './BasePage';
 import { LOCATORS } from '../locators';
-import { Locator, expect } from '@playwright/test';
+import { expect } from '@playwright/test';
 
 export class WikipediaPage extends BasePage {
   //searching the term on wikipedia page
@@ -25,18 +25,22 @@ export class WikipediaPage extends BasePage {
     this.verifyElementsToVisible(LOCATORS.externalLinksSectionLocator);
   }
 
+  //verifying that we are visiting correct url
   async verifyNavigationUrl(url: RegExp | string){
     expect(this.page).toHaveURL(url);
   }
 
+  //verifying text heading we are getting from website is same as expected header
   async verifyTextHeading(receievedHeading:string | null,expectedHeading:string) {
     expect(receievedHeading).toEqual(expectedHeading);
   }
 
+  //verifying text content we are receieving from website locator is same as expected test
   async verifyTextContent(receievedText:any,expectedText:string) {
     expect(receievedText).toContain(expectedText);
   }
 
+  //returns the text content from website locator
   async getTextContent(locator: any) {
     return await this.page.textContent(locator);
   }
